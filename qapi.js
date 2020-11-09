3790806532dfe9b43e107e100572ac38f787f72f
ronnymunozmartinez <48858227+ronnymunozmartinez@users.noreply.github.com>
2020-11-09 09:46:37 -0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fa939a379530b001fd229fd",
+				"5fa939a479530b001fd229ff"
+			]
+		},
+		"5fa939a379530b001fd229fd": {
+			"id": "5fa939a379530b001fd229fd",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fa939a479530b001fd229ff": {
+			"id": "5fa939a479530b001fd229ff",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
